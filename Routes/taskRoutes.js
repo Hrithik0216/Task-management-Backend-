24e41e17bbb3e2f62fa2d90d054e9a4d9e18620b
Hrithik Arun Kumar M <128972805+Hrithik0216@users.noreply.github.com>
2024-05-28 15:12:44 +0530

    Solved update issue

--- Routes/taskRoutes.js
+++ Routes/taskRoutes.js
@@ -5,7 +5,7 @@ const taskController = require("../controllers/taskController")
 router.post("/new-task", taskController.createTask) //validated if not found
 router.get("/all-tasks", taskController.getAlltasks) //validated if not found
 router.get("/get-a-task", taskController.getTaskById) //validated if not found
-router.post("/update-a-task", taskController.updateTask)
+router.post("/update-a-task", taskController.updateTask)//validated if not found
 router.delete("/delete-a-task", taskController.deleteTask)//validated if not found
 router.post("/mark-tasks-completed", taskController.markTasksCompleted)
 router.get("/based-on-priority", taskController.getTasksByPriority)//validated if not found
